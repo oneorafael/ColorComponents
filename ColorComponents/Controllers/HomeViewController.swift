b2902987e97ed8c86cf9487250fa443683254bc5
Rafael Oliveira <oliveirafael@icloud.com>
2023-05-15 15:19:06 -0300

    - Create Project - Remove Storyboard - Add HomeViewController as main viewController using viewCode on SceneDelegate

---
+++ ColorComponents/Controllers/HomeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  HomeViewController.swift
+//  ColorComponents
+//
+//  Created by Rafael Oliveira on 15/05/23.
+//
+
+import Foundation
+import UIKit
+
+class HomeViewController : UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        title = "Components"
+    }
+}
